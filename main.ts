e110985e93c43e29ebdeca6f9dd17e63e300c5ff
Joona Piirainen <joona.piirainen@gmail.com>
2022-07-17 13:13:23 +0300

    Chore: initialize

---
+++ main.ts
@@ -0,0 +1,25 @@
+import blog, {ga, redirects} from 'https://deno.land/x/blog@0.3.3/blog.tsx'
+import 'https://esm.sh/prismjs@1.27.0/components/prism-typescript?no-check'
+import 'https://esm.sh/prismjs@1.27.0/components/prism-haskell?no-check'
+import 'https://esm.sh/prismjs@1.27.0/components/prism-bash?no-check'
+import 'https://esm.sh/prismjs@1.27.0/components/prism-rust?no-check'
+
+blog({
+  title: 'Joona Piirainen',
+  author: 'Joona Piirainen',
+  avatar: './jp.jpg',
+  avatarClass: 'rounded-full',
+  links: [
+    {title: 'Email', url: 'mailto:joona.piirainen@gmail.org'},
+    {title: 'GitHub', url: 'https://github.com/japiirainen'},
+  ],
+  background: '#f9f9f9',
+  middlewares: [
+    // TODO
+    //ga("UA-91675022-1"),
+    redirects({
+      'iocp-links.html': 'iocp_links',
+      'rant.html': 'rant',
+    }),
+  ],
+})
